b5825456faab87cee4201bc86c4261a15f9b6285
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-16 19:21:23 -0400

    project-structure: created the project structure

---
+++ src/main/java/com/js/hmanager/HManagerApplication.java
@@ -0,0 +1,13 @@
+package com.js.hmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/js/hmanager/HManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.hmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
