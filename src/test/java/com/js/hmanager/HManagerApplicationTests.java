8bdafa0b5ac49705615d2097b0fc06ccbd9961dc
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-20 14:52:56 -0400

    refactor: disable `HManagerApplicationTests` contextLoads test

--- src/test/java/com/js/hmanager/HManagerApplicationTests.java
+++ src/test/java/com/js/hmanager/HManagerApplicationTests.java
@@ -1,12 +1,14 @@
 package com.js.hmanager;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HManagerApplicationTests {
 
-    @Test
+
+    //@Test
     void contextLoads() {
     }
 
