87ad48151d34a437ccdad58b3df9338cca9c3eed
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-20 14:51:13 -0400

    refactor: refactor the `InvalidArgumentDomainException` remove constructors

--- src/main/java/com/js/hmanager/sharad/domainExceptions/InvalidArgumentDomainException.java
+++ src/main/java/com/js/hmanager/sharad/domainExceptions/InvalidArgumentDomainException.java
@@ -1,22 +1,11 @@
 package com.js.hmanager.sharad.domainExceptions;
 
 public class InvalidArgumentDomainException extends RuntimeException {
-    public InvalidArgumentDomainException() {
-    }
-
     public InvalidArgumentDomainException(String message) {
         super(message);
     }
 
     public InvalidArgumentDomainException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public InvalidArgumentDomainException(Throwable cause) {
-        super(cause);
-    }
-
-    public InvalidArgumentDomainException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
