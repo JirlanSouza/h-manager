9e624e5e91d6446d0477bda186ad937a22746859
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-21 21:33:03 -0400

    feat: added the ``@CommandHandler` annotation on CreateCustomerHandler in handle method

--- src/main/java/com/js/hmanager/booking/domain/application/comandHandlers/CreateCustomerHandler.java
+++ src/main/java/com/js/hmanager/booking/domain/application/comandHandlers/CreateCustomerHandler.java
@@ -5,17 +5,23 @@ import com.js.hmanager.booking.domain.model.customer.Cpf;
 import com.js.hmanager.booking.domain.model.customer.Customer;
 import com.js.hmanager.booking.domain.model.customer.CustomerRepository;
 import com.js.hmanager.sharad.domainExceptions.ConflictEntityDomainException;
+import org.axonframework.commandhandling.CommandHandler;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class CreateCustomerHandler {
+
+    String id;
     private final CustomerRepository customerRepository;
 
     public CreateCustomerHandler(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
-    UUID handle(CreateCustomerCommand command) {
+    @CommandHandler
+    public UUID handle(CreateCustomerCommand command) {
         Cpf customerCpf = new Cpf(command.cpf());
         boolean existsCustomer = customerRepository.existsByCpf(customerCpf);
 
--- src/main/java/com/js/hmanager/booking/domain/application/comands/CreateCustomerCommand.java
+++ src/main/java/com/js/hmanager/booking/domain/application/comands/CreateCustomerCommand.java
@@ -2,4 +2,3 @@ package com.js.hmanager.booking.domain.application.comands;
 
 public record CreateCustomerCommand(String name, String cpf, CreateCustomerAddressDto address) {
 }
-
