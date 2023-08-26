083e39ea24c3dc33bde2269b717e41879902b6db
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-26 17:24:55 -0400

    refactor: move the `BookingRepository` to correct package

--- src/main/java/com/js/hmanager/booking/domain/model/booking/BookingRepository.java
+++ src/main/java/com/js/hmanager/booking/domain/model/booking/BookingRepository.java
@@ -1,7 +1,5 @@
 package com.js.hmanager.booking.domain.model.booking;
 
-import com.js.hmanager.booking.domain.model.booking.Booking;
-
 public interface BookingRepository {
     void save(Booking booking);
 }
