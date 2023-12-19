df2d3ede16c342f3b7c50ac0c8a1b2c91449b335
Jirlan Souza <jirlansouza08@gmail.com>
2023-12-18 22:00:54 -0400

    refactor: added the `@ResponseStatus` on the RestErrorsHandler methods to generate OpenAPI documentation

--- src/main/java/com/js/hmanager/config/errorHandlers/RestErrorsHandler.java
+++ src/main/java/com/js/hmanager/config/errorHandlers/RestErrorsHandler.java
@@ -8,21 +8,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class RestErrorsHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({InvalidArgumentDomainException.class,})
     public ResponseEntity<ProblemDetail> handleBadRequest(RuntimeException ex, WebRequest req) {
         return makeErrorResponse(HttpStatus.BAD_REQUEST, ex, req);
     }
 
+    @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(ConflictEntityDomainException.class)
     public ResponseEntity<ProblemDetail> handleConflict(RuntimeException ex, WebRequest req) {
         return makeErrorResponse(HttpStatus.CONFLICT, ex, req);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageConversionException.class)
     public ResponseEntity<ProblemDetail> handleHttpMessageError(RuntimeException ex, WebRequest req) {
         return makeErrorResponse(HttpStatus.BAD_REQUEST, new RuntimeException("Invalid request payload"), req);
