1891ec88686552343488f355f38bea2d62bd215f
Jirlan Souza <jirlansouza08@gmail.com>
2023-12-18 22:03:36 -0400

    refactor: added the Bean Validation annotations on the `CreateCustomerCommand`

--- src/main/java/com/js/hmanager/booking/domain/application/comands/CreateCustomerCommand.java
+++ src/main/java/com/js/hmanager/booking/domain/application/comands/CreateCustomerCommand.java
@@ -2,15 +2,21 @@ package com.js.hmanager.booking.domain.application.comands;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreateCustomerCommand(
         @NotBlank
+        @Size(min = 3, max = 70)
         String name,
         @NotNull
+        @Size(min = 11, max = 14)
         String cpf,
 
+        @Size(min = 10, max = 100)
         String email,
 
+        @NotNull()
+        @Size(min = 11, max = 15)
         String telephone,
         @NotNull
         CreateCustomerAddressDto address
