db388ebe2a27b740230e5f8696e618f755709fc9
Jirlan Souza <jirlansouza08@gmail.com>
2024-01-18 17:13:01 -0400

    test: created the restAssured Authentication with OAuth2 access token

--- src/test/java/com/js/hmanager/account/authentication/AuthenticationTestUtils.java
+++ src/test/java/com/js/hmanager/account/authentication/AuthenticationTestUtils.java
@@ -6,7 +6,17 @@ import com.js.hmanager.account.domain.User;
 import io.restassured.authentication.AuthenticationScheme;
 import io.restassured.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.oauth2;
@@ -17,14 +27,41 @@ public class AuthenticationTestUtils {
     @Autowired
     private JpaUserRepository jpaUserRepository;
 
+    @Autowired
+    JwtEncoder jwtEncoder;
+
     public AuthenticationScheme getAuthentication(int port) {
-        String token = this.getToken(port);
+        String token = this.getAccessToken();
         return oauth2(token);
     }
 
     public void createUser(String email, String password) {
         User user = User.create("Joe", "Jho", email, password, "MANAGER");
         this.jpaUserRepository.save(UserModel.from(user));
+    }
+
+    public String getAccessToken() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                "user",
+                "password");
+
+        Instant now = Instant.now();
+        long expiry = 36000L;
+
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("hmanager-backend")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+
+        Jwt jwt = this.jwtEncoder.encode(JwtEncoderParameters.from(claims));
+
+        return jwt.getTokenValue();
     }
 
     public String getToken(int port) {
