65755b812e54e7d80e5e80afdb84784423bbc99c
Jirlan Souza <jirlansouza08@gmail.com>
2024-01-21 21:37:43 -0400

    feat: created the Booking repository implementation

---
+++ src/main/java/com/js/hmanager/booking/booking/data/BookingRoomModel.java
@@ -0,0 +1,30 @@
+package com.js.hmanager.booking.booking.data;
+
+import com.js.hmanager.booking.booking.domain.BookingRoom;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bookings_rooms")
+public class BookingRoomModel {
+    @Id
+    private UUID id;
+    private String number;
+    private BigDecimal dailyRate;
+
+    @Column(name = "booking_id")
+    private UUID bookingId;
+
+    public BookingRoomModel(UUID bookingId, BookingRoom room) {
+        this(room.getId(), room.getNumber(), room.getDailyRate(), bookingId);
+    }
+}
