6c0d645a4c6899b1a8455655ed7f38f5a692df39
Jirlan Souza <jirlansouza08@gmail.com>
2024-02-04 21:53:34 -0400

    feat: create getBookingsSummary GET endpoint on `BookingsController`

--- src/main/java/com/js/hmanager/booking/booking/rest/BookingController.java
+++ src/main/java/com/js/hmanager/booking/booking/rest/BookingController.java
@@ -1,7 +1,11 @@
 package com.js.hmanager.booking.booking.rest;
 
+import com.js.hmanager.booking.booking.application.BookingQueryService;
+import com.js.hmanager.booking.booking.application.BookingSummary;
 import com.js.hmanager.booking.booking.application.CreateBooking;
 import com.js.hmanager.booking.booking.application.CreateBookingDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +16,21 @@ import java.util.UUID;
 public class BookingController {
 
     private final CreateBooking createBooking;
+    private final BookingQueryService bookingQueryService;
 
-    public BookingController(CreateBooking createBooking) {
+    public BookingController(CreateBooking createBooking, BookingQueryService bookingQueryService) {
         this.createBooking = createBooking;
+        this.bookingQueryService = bookingQueryService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UUID createBooking(@RequestBody CreateBookingDto bookingDto) {
         return this.createBooking.execute(bookingDto);
+    }
+
+    @GetMapping
+    public Page<BookingSummary> getBookingsSummary(Pageable pageable) {
+        return this.bookingQueryService.getBookingsSummary(pageable);
     }
 }
