d3e708a8ae2a6ab8071dfb5405a082084f1258a8
Jirlan Souza <jirlansouza08@gmail.com>
2024-02-09 20:25:57 -0400

    refactor: change `CpfTest` test methods to public and assertions to AssertJ

--- src/test/java/com/js/hmanager/reservation/customer/domain/CpfTest.java
+++ src/test/java/com/js/hmanager/reservation/customer/domain/CpfTest.java
@@ -4,51 +4,56 @@ import com.js.hmanager.common.domainExceptions.InvalidArgumentDomainException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CpfTest {
 
     @Test
     @DisplayName("Should create a new valid cpf")
-    void createNewCpf() {
+    public void createNewCpf() {
         var cpf = new Cpf("111.444.777-35");
-        assertEquals(cpf.value(), "111.444.777-35");
+        assertThat(cpf.value()).isEqualTo("111.444.777-35");
     }
 
     @Test
     @DisplayName("Should create a valid CPF with verifying digits 00")
-    void createNewCpfWith00VerifyingDigits() {
+    public void createNewCpfWith00VerifyingDigits() {
         var cpf = new Cpf("650.137.840-00");
-        assertEquals(cpf.value(), "650.137.840-00");
+        assertThat(cpf.value()).isEqualTo("650.137.840-00");
     }
 
     @Test
     @DisplayName("Should throw InvalidArgumentDomainException when creating cpf with null value")
-    void throwErrorWhenCreateCpfWithNullValue() {
-        assertThrows(InvalidArgumentDomainException.class, () -> new Cpf(null));
+    public void throwErrorWhenCreateCpfWithNullValue() {
+        assertThatThrownBy(() -> new Cpf(null))
+                .isInstanceOf(InvalidArgumentDomainException.class);
     }
     @Test
     @DisplayName("Should throw InvalidArgumentDomainException when creating invalid cpf")
-    void throwErrorWhenCreateInvalidCpf() {
-        assertThrows(InvalidArgumentDomainException.class, () -> new Cpf("011.444.777-35"));
+    public void throwErrorWhenCreateInvalidCpf() {
+        assertThatThrownBy(() -> new Cpf("011.444.777-35"))
+                .isInstanceOf(InvalidArgumentDomainException.class);
     }
 
     @Test
     @DisplayName("Should throw InvalidArgumentDomainException when creating cpf with no numbers characters")
-    void throwErrorWhenCreateCpfWithNoNumbersCharacters() {
-        assertThrows(InvalidArgumentDomainException.class, () -> new Cpf("cpf.cpf.cpf-in"));
+    public void throwErrorWhenCreateCpfWithNoNumbersCharacters() {
+        assertThatThrownBy(() -> new Cpf("cpf.cpf.cpf-in"))
+                .isInstanceOf(InvalidArgumentDomainException.class);
     }
 
     @Test
     @DisplayName("Should throw InvalidArgumentDomainException when creating cpf with more than 14 characters")
-    void throwErrorWhenCreateCpfWithMore11Characters() {
-        assertThrows(InvalidArgumentDomainException.class, () -> new Cpf("111.444.777.999"));
+    public void throwErrorWhenCreateCpfWithMore11Characters() {
+        assertThatThrownBy(() -> new Cpf("111.444.777.999"))
+                .isInstanceOf(InvalidArgumentDomainException.class);
     }
 
     @Test
     @DisplayName("Should throw InvalidArgumentDomainException when creating cpf with all equal characters")
-    void throwErrorWhenCreateCpfWithEqualsCharacters() {
-        assertThrows(InvalidArgumentDomainException.class, () -> new Cpf("111.111.111-11"));
+    public void throwErrorWhenCreateCpfWithEqualsCharacters() {
+        assertThatThrownBy(() -> new Cpf("111.111.111-11"))
+                .isInstanceOf(InvalidArgumentDomainException.class);
     }
 }
