ee089b3d283f2ca78cf970e2ec8341da21079e4c
Jirlan Souza <jirlansouza08@gmail.com>
2024-02-09 20:14:39 -0400

    refactor: change `CreateReservationServiceTest` assertions to AssertJ

--- src/test/java/com/js/hmanager/reservation/reservation/application/CreateReservationServiceTest.java
+++ src/test/java/com/js/hmanager/reservation/reservation/application/CreateReservationServiceTest.java
@@ -1,12 +1,12 @@
 package com.js.hmanager.reservation.reservation.application;
 
+import com.js.hmanager.common.domainExceptions.InvalidArgumentDomainException;
+import com.js.hmanager.common.domainExceptions.NotFoundEntityDomainException;
+import com.js.hmanager.reservation.customer.domain.CustomerRepository;
 import com.js.hmanager.reservation.reservation.application.adapters.InventoryService;
 import com.js.hmanager.reservation.reservation.domain.Reservation;
 import com.js.hmanager.reservation.reservation.domain.ReservationRepository;
 import com.js.hmanager.reservation.reservation.domain.ReservationRoom;
-import com.js.hmanager.reservation.customer.domain.CustomerRepository;
-import com.js.hmanager.common.domainExceptions.InvalidArgumentDomainException;
-import com.js.hmanager.common.domainExceptions.NotFoundEntityDomainException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,12 +19,12 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class CreateReservationTest {
+class CreateReservationServiceTest {
 
     @Mock
     private ReservationRepository reservationRepository;
@@ -36,12 +36,12 @@ class CreateReservationTest {
     InventoryService inventoryService;
 
     @InjectMocks
-    private CreateReservationService createReservationServiceHandler;
+    private CreateReservationService createReservationService;
 
     @Test
     @DisplayName("Should create a new reservation")
-    void createNewReservation() {
-        CreateReservationDto command = new CreateReservationDto(
+    public void createNewReservation() {
+        CreateReservationDto reservationDto = new CreateReservationDto(
                 UUID.randomUUID(),
                 OffsetDateTime.now(),
                 OffsetDateTime.now().plusDays(2),
@@ -52,28 +52,30 @@ class CreateReservationTest {
         when(inventoryService.findRooms(anyList()))
                 .thenReturn(List.of(new ReservationRoom("1001", new BigDecimal("220.99"))));
 
-        UUID reservationId = createReservationServiceHandler.execute(command);
+        UUID reservationId = createReservationService.execute(reservationDto);
 
-        assertNotNull(reservationId);
+        assertThat(reservationId).isNotNull();
         verify(reservationRepository, only()).save(isA(Reservation.class));
     }
 
     @Test
     @DisplayName("Should throw NotFoundEntityDomainException when creating a reservation with a no existent customer")
-    void createReservationWithNonexistentCustomer() {
-        CreateReservationDto command = new CreateReservationDto(
+    public void createReservationWithNonexistentCustomer() {
+        CreateReservationDto reservationDto = new CreateReservationDto(
                 UUID.randomUUID(),
                 OffsetDateTime.now(),
                 OffsetDateTime.now().plusDays(2),
                 List.of(UUID.randomUUID())
         );
 
-        assertThrows(NotFoundEntityDomainException.class, () -> createReservationServiceHandler.execute(command));
+        assertThatThrownBy(() -> createReservationService.execute(reservationDto))
+                .isInstanceOf(NotFoundEntityDomainException.class)
+                .hasMessage("The customer with id: '%s' does not exists".formatted(reservationDto.customerId()));
     }
 
     @Test
     @DisplayName("Should throw InvalidArgumentDomain exception when creating a reservation with an empty room list")
-    void createReservationWithEmptyRoomList() {
+    public void createReservationWithEmptyRoomList() {
         CreateReservationDto command = new CreateReservationDto(
                 UUID.randomUUID(),
                 OffsetDateTime.now(),
@@ -83,6 +85,8 @@ class CreateReservationTest {
 
         when(customerRepository.exists(isA(UUID.class))).thenReturn(true);
 
-        assertThrows(InvalidArgumentDomainException.class, () -> createReservationServiceHandler.execute(command));
+        assertThatThrownBy(() -> createReservationService.execute(command))
+                .isInstanceOf(InvalidArgumentDomainException.class)
+                .hasMessage("The rooms list must have one or more rooms");
     }
 }
